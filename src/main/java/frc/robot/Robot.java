6ec42bdef6aeee89b6d7618dfa9311316fb370b7
lyyleem <laurenlee4655@gmail.com>
2025-01-11 16:35:41 -0800

    pid tuning

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.Swerve;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class Robot extends TimedRobot {
@@ -31,6 +32,9 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     // System.out.println(configuration.toString());
     SmartDashboard.putNumber("Gyro", SwerveSubsystem.getInstance().getGyro());
+    SmartDashboard.putNumber("AnglePos", SwerveSubsystem.getInstance().getModuleStates()[0].angle.getDegrees());
+    SmartDashboard.putNumber("Speed", SwerveSubsystem.getInstance().getModuleStates()[0].speedMetersPerSecond);
+
     CommandScheduler.getInstance().run();
   }
 
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -41,7 +41,7 @@ import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
  * Basic simulation of a swerve subsystem with the methods needed by PathPlanner
  */
 public class SwerveSubsystem extends SubsystemBase {
-  double maximumSpeed = 4.5;
+  double maximumSpeed = 6;
   File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
   SwerveDrive swerveDrive;
   private Field2d field = new Field2d();
