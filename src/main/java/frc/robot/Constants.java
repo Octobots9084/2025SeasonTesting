86e31f0b9dc585e849f6032e3e776ec104ba76d5
lyyleem <laurenlee4655@gmail.com>
2025-01-13 20:39:21 -0800

    mainly working

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,7 +37,7 @@ public final class Constants {
     public static final Translation2d blModuleOffset = new Translation2d(-0.64135 / 2.0, 0.64135 / 2.0);
     public static final Translation2d brModuleOffset = new Translation2d(-0.64135 / 2.0, -0.64135 / 2.0);
 
-    public static final double maxModuleSpeed = 4.5; // M/S
+    public static final double maxModuleSpeed = 47; // M/S
 
     public static final PIDConstants translationConstants = new PIDConstants(5, 0.0, 0.0);
     public static final PIDConstants rotationConstants = new PIDConstants(5, 0.0, 0.0);
